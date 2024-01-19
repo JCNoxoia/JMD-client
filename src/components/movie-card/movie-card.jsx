f5a27115476f1bdc6c48a6cdd8586bb2035dec7d
JCNoxoia <jcbarrett2000@protonmail.com>
2024-01-19 16:29:24 -0600

    endpoints now pull from api and added proptypes

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,3 +1,5 @@
+import PropTypes from "prop-types";
+
 export const MovieCard = ({ movie, onMovieClick }) => {
   return (
     <div
@@ -8,4 +10,19 @@ export const MovieCard = ({ movie, onMovieClick }) => {
       {movie.title}
     </div>
   );
+};
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    releaseYear: PropTypes.string,
+    director: PropTypes.shape({
+      name: PropTypes.string.isRequired
+    }),
+    genre: PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      description: PropTypes.string
+    })
+  }).isRequired,
+  onMovieClick: PropTypes.func.isRequired
 };
